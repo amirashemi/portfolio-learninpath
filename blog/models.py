7aa206f1614a1a997e88761b111b3b4cd979e88b
amir <samirhashemi@protonmail.com>
2019-06-27 15:40:29 +0430

    add blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class blogpost(models.Model):
+    title = models.CharField(max_length=75)
+
+    pub_date = models.DateTimeField()
+
+    body = models.TextField()
+
+    image = models.ImageField(upload_to='images/blogimages')
